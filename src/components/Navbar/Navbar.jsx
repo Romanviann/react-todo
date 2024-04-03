7d4a410fee877c11724a063d55b6bf90a9426fac
Omar Jawhar <omarjawhar.10@gmail.com>
2024-04-03 18:20:45 -0400

    Setup app navbar component and structure
    added react logo to assets + created navbar component with basic setup and styling

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,11 +1,18 @@
 import React from "react";
 import "./Navbar.css";
+import ReactLogo from "./../../assets/images/react.png"
 
 
 export default function Navbar() {
     return (
         <nav className="navbar">
-            <h3>React To-Do</h3>
+            <div className="navbar-content poppins-medium">
+                <img src={ReactLogo} width="50px" alt="react-logo" />
+                <h2>React To-Do</h2>
+            </div>
+            <div className="reddit-mono-regular">
+                To-Do List
+            </div>
         </nav>
     )
 }
