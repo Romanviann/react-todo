5a842cb9780a8e1ef8fe85083c32d5b4dd9ca616
Omar Jawhar <omarjawhar.10@gmail.com>
2024-04-03 17:24:07 -0400

    Initial Commit
    App setup using Vite.

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+
+import './App.css'
+import Navbar from "./components/Navbar/Navbar.jsx";
+
+function App() {
+
+  return (
+    <>
+        <Navbar></Navbar>
+        <h1>Hello World</h1>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./Navbar.css";
+
+
+export default function Navbar() {
+    return (
+        <nav className="navbar">
+            <h3>React To-Do</h3>
+        </nav>
+    )
+}
